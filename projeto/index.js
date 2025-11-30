e056718a440d826d5dfb73e6afb986fed108950d
Tiago Barbosa <sombr43@gmail.com>
2025-11-30 14:17:00 -0300

    first commit

---
+++ projeto/index.js
@@ -0,0 +1,33 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    scalar Date
+
+    # Pontos de entra da sua API!
+    type Query {
+        ola: String
+        horaAtual: Date
+    }
+`
+
+const resolvers = {
+  Query: {
+    ola() {
+      return 'Bom dia!'
+    },
+
+    horaAtual(){
+      return new Date
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Executando em ${url}`)
+})
+
