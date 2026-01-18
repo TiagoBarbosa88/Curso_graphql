50eaf1718a40f0b9dc6a957bb00ed88f0c7b5950
Tiago Barbosa <sombr43@gmail.com>
2026-01-18 12:57:45 -0300

    feat: Add Enum, tipando um data

--- projeto/data/db.js
+++ projeto/data/db.js
@@ -15,21 +15,24 @@ const usuarios = [
     nome: 'Jose Silva',
     email: 'jsilva@email.com',
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
   },
   {
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafaeljr@zemail.com',
     idade: 31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
   },
   {
     id: 3,
     nome: 'Daniela Smith',
     email: 'danismith@uemail.com',
     idade: 24,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
   }
 ];
 
