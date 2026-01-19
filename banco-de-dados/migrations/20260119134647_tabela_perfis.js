29163ed3bee6e028f95e3b07a389e6862f796d6f
Tiago Barbosa <sombr43@gmail.com>
2026-01-19 11:14:09 -0300

    feat: Criação da tabela de perfis com dados mocados

--- banco-de-dados/migrations/20260119134647_tabela_perfis.js
+++ banco-de-dados/migrations/20260119134647_tabela_perfis.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('perfis', table => {
+    table.increments('id').primary();
+    table.string('nome').notNullable().unique();
+    table.string('rotulo').notNullable().unique();
+  }).then(function () {
+    return knex('perfis').insert([
+      { nome: 'comum', rotulo: 'Comum' },
+      { nome: 'admin', rotulo: 'Administrador' },
+      { nome: 'master', rotulo: 'Master' },
+    ]);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('perfis');
 };
